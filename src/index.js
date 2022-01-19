e71d656d42b288c77e55a5d95ab720508e04cbb9
Joe Goggins <joe@particle.io>
2022-01-18 19:02:27 -0600

    Implement getDefinitions()

--- src/index.js
+++ src/index.js
@@ -1,33 +1,68 @@
 const fs = require('fs');
 const path = require('path');
 const pbjsGeneratedProtobufCode = require('./pbjs-generated/index');
-const _ = require('lodash');
 
 class DeviceOSProtobuf {
-    static getDefinition(protobufMessageName) {
-        const id = this._getIDFromJSON(protobufMessageName);
-        const message = this._getProtobufMessage(protobufMessageName);
-    
-        return {
-            id,
-            message
-        };
-    }
 
-    static _getIDFromJSON(protobufMessageName) {
-        try {
-            return DeviceOSProtobuf._pbjsJSON.nested.particle.nested.ctrl.nested[protobufMessageName].options['(type_id)'];
-        } catch (e) {
-            throw new Error(`Could not extract request type id from name=${protobufMessageName}`);
-        }
-    }
+	/**
+	 *
+	 * @param {String} protobufMessageName Name of the Protobuf, enum, etc from *.proto file
+	 * @returns {Function} protobufjs jbjs generated Function with encode, create, decode methods
+	 */
+	static getDefinition(protobufMessageName) {
+		const id = this._getIDFromJSON(protobufMessageName);
+		const message = this._getProtobufMessage(protobufMessageName);
 
-    static _getProtobufMessage(protobufMessageName) {
-    	if (!(protobufMessageName in this._pbjsObjects)) {
-			throw new Error(`There is no protobuf request function called "${protobufMessageName}"`);
+		return {
+			id,
+			message
+		};
+	}
+
+	/**
+	 *
+	 * @returns {Array} all valid arguments to getDefinition()
+	 */
+	static getDefinitions() {
+		const namespaces = this.getNamespaces();
+		const returnThis = [];
+		for (const key of Object.keys(this._pbjsObjects)) {
+			if (namespaces.includes(key)) {
+				const namespace = namespaces[namespaces.indexOf(key)];
+				for (const namespacedKey of Object.keys(this._pbjsObjects[namespace])) {
+					returnThis.push(`${namespace}.${namespacedKey}`);
+				}
+			} else {
+				returnThis.push(key);
+			}
 		}
-		return this._pbjsObjects[protobufMessageName];    
-    }
+		return returnThis;
+	}
+
+	static getNamespaces() {
+		return [
+			'wifi',
+			'logging',
+			'mesh',
+			'cloud',
+			'cellular'
+		];
+	}
+
+	static _getIDFromJSON(protobufMessageName) {
+		try {
+			return DeviceOSProtobuf._pbjsJSON.nested.particle.nested.ctrl.nested[protobufMessageName].options['(type_id)'];
+		} catch (e) {
+			throw new Error(`Could not extract request type id from pbjs json where protobufMessageName=${protobufMessageName}`);
+		}
+	}
+
+	static _getProtobufMessage(protobufMessageName) {
+		if (!(protobufMessageName in this._pbjsObjects)) {
+			throw new Error(`There is no pbjs generated protobuf Function for protobufMessageName=${protobufMessageName}`);
+		}
+		return this._pbjsObjects[protobufMessageName];
+	}
 }
 
 const pbjsJSONString = fs.readFileSync(`${path.resolve(__dirname)}/pbjs-generated/index.json`);
--- src/index.test.js
+++ src/index.test.js
@@ -3,60 +3,302 @@ const sinon = require('sinon');
 const DeviceOSProtobuf = require('./');
 
 describe('deviceOSProtobuf', () => {
-    let protobufMessageName, requestID;
+	let protobufMessageName, requestID;
 
-    afterEach(() => {
-        sinon.restore();
-    });
+	afterEach(() => {
+		sinon.restore();
+	});
 
-    describe('protobufMessageName(protobufMessageName) without namespace and generalized usage', () => {
-        beforeEach(() => {
-            protobufMessageName = 'GetSerialNumberRequest'
-            requestID = 21;
-        });        
-        it('provides getDefinition(protobufMessageName)', () => {
-            const stubID = 47;
-            sinon.stub(DeviceOSProtobuf, '_getIDFromJSON').returns(stubID);
-            const stubMessage = function() {};
-            stubMessage.create = sinon.stub();
-            stubMessage.encode = sinon.stub();
-            stubMessage.decode = sinon.stub();
-            sinon.stub(DeviceOSProtobuf, '_getProtobufMessage').returns(stubMessage);
-            const protobufDefinition = DeviceOSProtobuf.getDefinition(protobufMessageName);
-            expect(protobufDefinition).to.have.haveOwnProperty('id');
-            expect(protobufDefinition).to.have.haveOwnProperty('message');
-            expect(DeviceOSProtobuf._getIDFromJSON).to.have.property('callCount', 1);
-            expect(DeviceOSProtobuf._getProtobufMessage).to.have.property('callCount', 1);
-        });
+	describe('getDefinition(protobufMessageName) without protobuf namespace', () => {
+		beforeEach(() => {
+			protobufMessageName = 'GetSerialNumberRequest';
+			requestID = 21;
+		});
+		it('provides getDefinition(protobufMessageName)', () => {
+			const stubID = 47;
+			sinon.stub(DeviceOSProtobuf, '_getIDFromJSON').returns(stubID);
+			const stubMessage = () => {};
+			stubMessage.create = sinon.stub();
+			stubMessage.encode = sinon.stub();
+			stubMessage.decode = sinon.stub();
+			sinon.stub(DeviceOSProtobuf, '_getProtobufMessage').returns(stubMessage);
+			const protobufDefinition = DeviceOSProtobuf.getDefinition(protobufMessageName);
+			expect(protobufDefinition).to.have.haveOwnProperty('id');
+			expect(protobufDefinition).to.have.haveOwnProperty('message');
+			expect(DeviceOSProtobuf._getIDFromJSON).to.have.property('callCount', 1);
+			// expect(DeviceOSProtobuf._getProtobufMessage).to.have.property('callCount', 1);
+		});
 
-        it('implements _getIDFromJSON(protobufMessageName) correctly', () => {
-            const result = DeviceOSProtobuf._getIDFromJSON(protobufMessageName);
-            expect(result).to.eql(requestID);
-        });
+		it('implements _getIDFromJSON(protobufMessageName) correctly', () => {
+			const result = DeviceOSProtobuf._getIDFromJSON(protobufMessageName);
+			expect(result).to.eql(requestID);
 
-        it('implements _getProtobufMessage(protobufMessageName) correctly', () => {
-            const result = DeviceOSProtobuf._getProtobufMessage(protobufMessageName);
-            assertValidProtobufMessage(result);
-        });
-    })
-	
-    it('provides _pbjsJSON (parsed JSON rendered via `npm run build:json` command)', () => {
-        expect(DeviceOSProtobuf._pbjsJSON).to.be.an('object');
-    });
+			let error;
+			try {
+				DeviceOSProtobuf._getIDFromJSON('foobar');
+			} catch (e) {
+				error = e;
+			}
+			expect(error).to.be.an.instanceOf(Error);
+			expect(error.message).to.eql(
+				`Could not extract request type id from pbjs json where protobufMessageName=foobar`
+			);
+		});
 
-    it('provides _pbjsObjects (generated Javascript via `npm run build` command)', () => {
-        expect(DeviceOSProtobuf._pbjsObjects).to.be.an('object');
-        expect(DeviceOSProtobuf._pbjsObjects).to.have.property('GetSerialNumberRequest');
-    });
+		it('implements _getProtobufMessage(protobufMessageName) correctly', () => {
+			const result = DeviceOSProtobuf._getProtobufMessage(protobufMessageName);
+			assertValidProtobufMessage(result);
+
+			let error;
+			try {
+				DeviceOSProtobuf._getProtobufMessage('foobar');
+			} catch (e) {
+				error = e;
+			}
+			expect(error).to.be.an.instanceOf(Error);
+			expect(error.message).to.eql(
+				'There is no pbjs generated protobuf Function for protobufMessageName=foobar'
+			);
+		});
+	});
+
+	describe('getDefinition(protobufMessageName) with protobuf namespace', () => {
+		beforeEach(() => {
+			protobufMessageName = 'cellular.GetIccidRequest';
+			requestID = 99; // TODO
+		});
+
+		it('provides getDefinition(namespace.protobufMessageName)', () => {
+			// TODO
+			expect(true).to.eql(true);
+		});
+	});
+
+
+	describe('getDefinitions()', () => {
+		it('provides getDefinitions()', () => {
+			const definitions = DeviceOSProtobuf.getDefinitions();
+			expect(definitions).to.be.an('Array');
+			expect(definitions).to.eql([
+				'cellular.SimType',
+				'cellular.AccessPoint',
+				'cellular.SetAccessPointRequest',
+				'cellular.SetAccessPointReply',
+				'cellular.GetAccessPointRequest',
+				'cellular.GetAccessPointReply',
+				'cellular.SetActiveSimRequest',
+				'cellular.SetActiveSimReply',
+				'cellular.GetActiveSimRequest',
+				'cellular.GetActiveSimReply',
+				'cellular.GetIccidRequest',
+				'cellular.GetIccidReply',
+				'ResultCode',
+				'Ipv4Address',
+				'Ipv6Address',
+				'IpAddress',
+				'IPAddress',
+				'cloud.ConnectionStatus',
+				'cloud.GetConnectionStatusRequest',
+				'cloud.GetConnectionStatusReply',
+				'cloud.ConnectRequest',
+				'cloud.ConnectReply',
+				'cloud.DisconnectRequest',
+				'cloud.DisconnectReply',
+				'SecurityKeyType',
+				'ServerProtocolType',
+				'DeviceMode',
+				'SystemCapabilityFlag',
+				'Feature',
+				'GetDeviceIdRequest',
+				'GetDeviceIdReply',
+				'GetSerialNumberRequest',
+				'GetSerialNumberReply',
+				'GetSystemVersionRequest',
+				'GetSystemVersionReply',
+				'GetNcpFirmwareVersionRequest',
+				'GetNcpFirmwareVersionReply',
+				'GetSystemCapabilitiesRequest',
+				'GetSystemCapabilitiesReply',
+				'SetClaimCodeRequest',
+				'SetClaimCodeReply',
+				'IsClaimedRequest',
+				'IsClaimedReply',
+				'SetSecurityKeyRequest',
+				'SetSecurityKeyReply',
+				'GetSecurityKeyRequest',
+				'GetSecurityKeyReply',
+				'SetServerAddressRequest',
+				'SetServerAddressReply',
+				'GetServerAddressRequest',
+				'GetServerAddressReply',
+				'SetServerProtocolRequest',
+				'SetServerProtocolReply',
+				'GetServerProtocolRequest',
+				'GetServerProtocolReply',
+				'SetSoftApSsidRequest',
+				'SetSoftApSsidReply',
+				'StartListeningModeRequest',
+				'StartListeningModeReply',
+				'StopListeningModeRequest',
+				'StopListeningModeReply',
+				'GetDeviceModeRequest',
+				'GetDeviceModeReply',
+				'SetDeviceSetupDoneRequest',
+				'SetDeviceSetupDoneReply',
+				'IsDeviceSetupDoneRequest',
+				'IsDeviceSetupDoneReply',
+				'SetStartupModeRequest',
+				'SetStartupModeReply',
+				'SystemResetRequest',
+				'SystemResetReply',
+				'SetFeatureRequest',
+				'SetFeatureReply',
+				'GetFeatureRequest',
+				'GetFeatureReply',
+				'StartNyanSignalRequest',
+				'StartNyanSignalReply',
+				'StopNyanSignalRequest',
+				'StopNyanSignalReply',
+				'WiFiGetAntennaRequest',
+				'WiFiGetAntennaReply',
+				'WiFiSetAntennaRequest',
+				'WiFiSetAntennaReply',
+				'WiFiScanRequest',
+				'WiFiScanReply',
+				'WiFiGetCredentialsRequest',
+				'WiFiGetCredentialsReply',
+				'WiFiSetCredentialsRequest',
+				'WiFiSetCredentialsReply',
+				'WiFiClearCredentialsRequest',
+				'WiFiClearCredentialsReply',
+				'WiFiAntenna',
+				'WiFiSecurityType',
+				'WiFiSecurityCipher',
+				'EapType',
+				'WiFiAccessPoint',
+				'WiFiAccessPointList',
+				'NetworkGetStatusRequest',
+				'NetworkGetStatusReply',
+				'NetworkGetConfigurationRequest',
+				'NetworkGetConfigurationReply',
+				'NetworkSetConfigurationRequest',
+				'NetworkSetConfigurationReply',
+				'NetworkState',
+				'IPConfiguration',
+				'DNSConfiguration',
+				'NetworkConfiguration',
+				'InterfaceAddress',
+				'Ipv4Config',
+				'Ipv6Config',
+				'InterfaceFlag',
+				'InterfaceExtFlag',
+				'InterfaceType',
+				'Interface',
+				'InterfaceEntry',
+				'GetInterfaceListRequest',
+				'GetInterfaceListReply',
+				'GetInterfaceRequest',
+				'GetInterfaceReply',
+				'wifi.Security',
+				'wifi.CredentialsType',
+				'wifi.Credentials',
+				'wifi.JoinNewNetworkRequest',
+				'wifi.JoinNewNetworkReply',
+				'wifi.JoinKnownNetworkRequest',
+				'wifi.JoinKnownNetworkReply',
+				'wifi.GetKnownNetworksRequest',
+				'wifi.GetKnownNetworksReply',
+				'wifi.RemoveKnownNetworkRequest',
+				'wifi.RemoveKnownNetworkReply',
+				'wifi.ClearKnownNetworksRequest',
+				'wifi.ClearKnownNetworksReply',
+				'wifi.GetCurrentNetworkRequest',
+				'wifi.GetCurrentNetworkReply',
+				'wifi.ScanNetworksRequest',
+				'wifi.ScanNetworksReply',
+				'logging.LogLevel',
+				'logging.LogHandlerType',
+				'logging.StreamType',
+				'logging.LogFilter',
+				'logging.SerialStreamParams',
+				'logging.AddLogHandlerRequest',
+				'logging.AddLogHandlerReply',
+				'logging.RemoveLogHandlerRequest',
+				'logging.RemoveLogHandlerReply',
+				'logging.GetLogHandlersRequest',
+				'logging.GetLogHandlersReply',
+				'mesh.NetworkInfo',
+				'mesh.AuthRequest',
+				'mesh.AuthReply',
+				'mesh.CreateNetworkRequest',
+				'mesh.CreateNetworkReply',
+				'mesh.StartCommissionerRequest',
+				'mesh.StartCommissionerReply',
+				'mesh.StopCommissionerRequest',
+				'mesh.StopCommissionerReply',
+				'mesh.PrepareJoinerRequest',
+				'mesh.PrepareJoinerReply',
+				'mesh.AddJoinerRequest',
+				'mesh.AddJoinerReply',
+				'mesh.RemoveJoinerRequest',
+				'mesh.RemoveJoinerReply',
+				'mesh.JoinNetworkRequest',
+				'mesh.JoinNetworkReply',
+				'mesh.LeaveNetworkRequest',
+				'mesh.LeaveNetworkReply',
+				'mesh.GetNetworkInfoRequest',
+				'mesh.GetNetworkInfoReply',
+				'mesh.ScanNetworksRequest',
+				'mesh.ScanNetworksReply',
+				'mesh.DiagnosticType',
+				'mesh.DiagnosticInfo',
+				'mesh.GetNetworkDiagnosticsRequest',
+				'mesh.GetNetworkDiagnosticsReply',
+				'StorageType',
+				'SectionType',
+				'FirmwareModuleType',
+				'FirmwareModuleValidityFlag',
+				'SectionFlag',
+				'FileFormat',
+				'StartFirmwareUpdateRequest',
+				'StartFirmwareUpdateReply',
+				'FinishFirmwareUpdateRequest',
+				'FinishFirmwareUpdateReply',
+				'CancelFirmwareUpdateRequest',
+				'CancelFirmwareUpdateReply',
+				'FirmwareUpdateDataRequest',
+				'FirmwareUpdateDataReply',
+				'DescribeStorageRequest',
+				'DescribeStorageReply',
+				'ReadSectionDataRequest',
+				'ReadSectionDataReply',
+				'WriteSectionDataRequest',
+				'WriteSectionDataReply',
+				'ClearSectionDataRequest',
+				'ClearSectionDataReply',
+				'GetSectionDataSizeRequest',
+				'GetSectionDataSizeReply',
+				'GetModuleInfoRequest',
+				'GetModuleInfoReply'
+			]);
+		});
+	});
+
+	it('provides _pbjsJSON (parsed JSON rendered via `npm run build:json` command)', () => {
+		expect(DeviceOSProtobuf._pbjsJSON).to.be.an('object');
+	});
+
+	it('provides _pbjsObjects (generated Javascript via `npm run build` command)', () => {
+		expect(DeviceOSProtobuf._pbjsObjects).to.be.an('object');
+		expect(DeviceOSProtobuf._pbjsObjects).to.have.property('GetSerialNumberRequest');
+	});
 });
 
-/**
- * Test helper function that validates the pbjs generated JS code returns an
- * object with the structure we expect.
- */
+
+// Test helper function to validate  pbjs generated JS code returns object like we expect
 function assertValidProtobufMessage(message) {
-    expect(message).to.be.a('Function');
-    expect(message.create).to.be.a('Function');
-    expect(message.encode).to.be.a('Function');
-    expect(message.decode).to.be.a('Function');
+	expect(message).to.be.a('Function');
+	expect(message.create).to.be.a('Function');
+	expect(message.encode).to.be.a('Function');
+	expect(message.decode).to.be.a('Function');
 }
